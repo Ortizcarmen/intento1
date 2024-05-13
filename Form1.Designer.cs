b05459712e31f3b8f4569ed42364561632767787
Ortizcarmen <156687528+Ortizcarmen@users.noreply.github.com>
2024-05-13 03:32:48 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace intento1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNuevoCarne = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtCarneactual = new System.Windows.Forms.TextBox();
+            this.bottonqr = new System.Windows.Forms.Button();
+            this.TxtNuevoCarne = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.pictureBoxqr = new System.Windows.Forms.PictureBox();
+            this.comboBoxCarrera = new System.Windows.Forms.ComboBox();
+            this.buttonLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxqr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNuevoCarne
+            // 
+            this.btnNuevoCarne.Location = new System.Drawing.Point(182, 232);
+            this.btnNuevoCarne.Name = "btnNuevoCarne";
+            this.btnNuevoCarne.Size = new System.Drawing.Size(227, 23);
+            this.btnNuevoCarne.TabIndex = 0;
+            this.btnNuevoCarne.Text = "Nuevo número de carné:";
+            this.btnNuevoCarne.UseVisualStyleBackColor = true;
+            this.btnNuevoCarne.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese el numero de carne actual:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(361, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fecha:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(361, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Carrera:";
+            // 
+            // TxtCarneactual
+            // 
+            this.TxtCarneactual.Location = new System.Drawing.Point(570, 38);
+            this.TxtCarneactual.Name = "TxtCarneactual";
+            this.TxtCarneactual.Size = new System.Drawing.Size(190, 22);
+            this.TxtCarneactual.TabIndex = 4;
+            // 
+            // bottonqr
+            // 
+            this.bottonqr.Location = new System.Drawing.Point(166, 344);
+            this.bottonqr.Name = "bottonqr";
+            this.bottonqr.Size = new System.Drawing.Size(117, 23);
+            this.bottonqr.TabIndex = 7;
+            this.bottonqr.Text = "Codigo QR";
+            this.bottonqr.UseVisualStyleBackColor = true;
+            this.bottonqr.Click += new System.EventHandler(this.bottonqr_Click);
+            // 
+            // TxtNuevoCarne
+            // 
+            this.TxtNuevoCarne.Location = new System.Drawing.Point(570, 233);
+            this.TxtNuevoCarne.Name = "TxtNuevoCarne";
+            this.TxtNuevoCarne.Size = new System.Drawing.Size(190, 22);
+            this.TxtNuevoCarne.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(515, 108);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(245, 22);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // pictureBoxqr
+            // 
+            this.pictureBoxqr.Location = new System.Drawing.Point(347, 308);
+            this.pictureBoxqr.Name = "pictureBoxqr";
+            this.pictureBoxqr.Size = new System.Drawing.Size(413, 262);
+            this.pictureBoxqr.TabIndex = 10;
+            this.pictureBoxqr.TabStop = false;
+            // 
+            // comboBoxCarrera
+            // 
+            this.comboBoxCarrera.FormattingEnabled = true;
+            this.comboBoxCarrera.Items.AddRange(new object[] {
+            "Ingeniería en Sistemas",
+            "Ingeniería en Electrónica",
+            "Ingeniería en Telecomunicaciones"});
+            this.comboBoxCarrera.Location = new System.Drawing.Point(570, 170);
+            this.comboBoxCarrera.Name = "comboBoxCarrera";
+            this.comboBoxCarrera.Size = new System.Drawing.Size(190, 24);
+            this.comboBoxCarrera.TabIndex = 11;
+            this.comboBoxCarrera.SelectedIndexChanged += new System.EventHandler(this.comboBoxCarrera_SelectedIndexChanged);
+            // 
+            // buttonLimpiar
+            // 
+            this.buttonLimpiar.Location = new System.Drawing.Point(166, 432);
+            this.buttonLimpiar.Name = "buttonLimpiar";
+            this.buttonLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLimpiar.TabIndex = 12;
+            this.buttonLimpiar.Text = "Limpiar";
+            this.buttonLimpiar.UseVisualStyleBackColor = true;
+            this.buttonLimpiar.Click += new System.EventHandler(this.buttonLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 635);
+            this.Controls.Add(this.buttonLimpiar);
+            this.Controls.Add(this.comboBoxCarrera);
+            this.Controls.Add(this.pictureBoxqr);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.TxtNuevoCarne);
+            this.Controls.Add(this.bottonqr);
+            this.Controls.Add(this.TxtCarneactual);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNuevoCarne);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxqr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNuevoCarne;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtCarneactual;
+        private System.Windows.Forms.Button bottonqr;
+        private System.Windows.Forms.TextBox TxtNuevoCarne;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.PictureBox pictureBoxqr;
+        private System.Windows.Forms.ComboBox comboBoxCarrera;
+        private System.Windows.Forms.Button buttonLimpiar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing.Common;
+using ZXing;
+using ZXing.QrCode;
+
+namespace intento1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Verificar si se ha seleccionado una carrera en el ComboBox
+            if (comboBoxCarrera.SelectedItem == null)
+            {
+                MessageBox.Show("Por favor seleccione una carrera");
+                return;
+            }
+
+            // Obtener el año seleccionado del DateTimePicker
+            int año = dateTimePicker1.Value.Year;
+
+            // Obtener el código de carrera según la selección del ComboBox
+            string codigoCarrera = "";
+            switch (comboBoxCarrera.SelectedItem.ToString())
+            {
+                case "Ingeniería en Sistemas":
+                    codigoCarrera = "08";
+                    break;
+                case "Ingeniería en Electrónica":
+                    codigoCarrera = "07";
+                    break;
+                case "Ingeniería en Telecomunicaciones":
+                    codigoCarrera = "14";
+                    break;
+                // Agrega más casos según sea necesario para otras opciones de carrera
+                default:
+                    // Esto no debería ocurrir si las opciones del ComboBox están bien definidas
+                    MessageBox.Show("Carrera seleccionada no válida");
+                    return;
+            }
+
+            // Obtener el número de carné actual del TextBox
+            string carneActual = TxtCarneactual.Text;
+
+            // Obtener los últimos 3 dígitos del número de carné actual
+            string ultimosDigitos = carneActual.Substring(carneActual.Length - 3);
+
+            // Construir el nuevo número de carné
+            string nuevoCarne = $"{año}{codigoCarrera}{ultimosDigitos}";
+
+            // Mostrar el nuevo número de carné en el TextBox correspondiente
+            TxtNuevoCarne.Text = nuevoCarne;
+
+            // Generar y mostrar el código UPC basado en el nuevo carné
+            MostrarCodigoUPC(nuevoCarne);
+
+            // Generar y mostrar el código QR basado en el nuevo carné
+            MostrarCodigoQR(nuevoCarne);
+        }
+
+        private void comboBoxCarrera_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bottonqr_Click(object sender, EventArgs e)
+        {
+            // Verificar si se ha ingresado un número de carné válido en el TextBox
+            if (string.IsNullOrEmpty(TxtNuevoCarne.Text))
+            {
+                MessageBox.Show("Por favor genera un número de carné primero");
+                return;
+            }
+
+            // Obtener el número de carné generado
+            string numeroCarne = TxtNuevoCarne.Text;
+
+            // Generar y mostrar el código UPC basado en el número de carné
+            MostrarCodigoUPC(numeroCarne);
+        }
+
+        private void MostrarCodigoUPC(string numeroCarne)
+        {
+            // Asegurar que el número de carné tenga 12 dígitos agregando ceros al principio si es necesario
+            string numeroCarneConCeros = numeroCarne.PadLeft(12, '0');
+
+            // Crear un objeto de configuración para el código UPC
+            var options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = pictureBoxqr.Width,
+                Height = pictureBoxqr.Height,
+                Margin = 0
+            };
+
+            BarcodeWriter writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.UPC_A;
+            writer.Options = options;
+
+            // Generar el código UPC basado en el número de carné con ceros adicionales
+            Bitmap codigoUPC = writer.Write(numeroCarneConCeros);
+
+            // Mostrar el código UPC en el PictureBox
+            pictureBoxqr.Image = codigoUPC;
+        }
+
+
+        private void MostrarCodigoQR(string numeroCarne)
+        {
+            // Crear un objeto de configuración para el código QR
+            BarcodeWriter writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+
+            // Generar el código QR basado en el número de carné
+            Bitmap codigoQR = writer.Write(numeroCarne);
+
+            // Mostrar el código QR en el PictureBox
+            pictureBoxqr.Image = codigoQR;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLimpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+    
+}
+
